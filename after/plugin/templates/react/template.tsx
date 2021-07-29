1484c331fee2fa548271df21e4910902f6f305eb
fudesign2008 <fudesign2008@163.com>
2021-07-29 16:05:55 +0800

    new file:   react/index.ts         new file:   react/template.css         new file:   react/template.less         new file:   react/template.scss         new file:   react/template.tsx

---
+++ after/plugin/templates/react/template.tsx
@@ -0,0 +1,40 @@
+/**
+ *
+ * @author fuyg
+ * @date  CREATE_DATE
+ */
+
+
+import React, { ReactElement, useEffect } from 'react'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
+import styles from './ComponentName.MIDDLE_NAME.scss'
+import { fetchData } from '../sliceName'
+import { RootState } from 'src/pages/home/store/rootReducer'
+
+export interface ComponentNameProps {
+  propName: string
+}
+
+
+function ComponentName(props: ComponentNameProps): ReactElement {
+  // 获取数据
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchData({}))
+  }, [])
+
+  // 绑定数据
+  const { data } = useSelector((state: RootState) => {
+    return {
+      data: state.sliceName.data,
+    }
+  }, shallowEqual)
+
+  // TODO
+
+  return <div className={styles.container}></div>
+}
+
+export default ComponentName
+
