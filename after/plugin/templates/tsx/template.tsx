f36df9849f2bd2459f10ae18f23b3615b2ae3019
fudesign2008 <fudesign2008@163.com>
2023-09-25 20:54:10 +0800

    modified:   after/plugin/templates/tsx/template.tsx

--- after/plugin/templates/tsx/template.tsx
+++ after/plugin/templates/tsx/template.tsx
@@ -1,5 +1,10 @@
-import styles from './ComponentName.module.scss';
+import styles from './ComponentName.module.scss'
+import classnames from 'classnames'
 
 export const ComponentName = () => {
-  return <div className={styles.componentName}>组件 ComponentName 待实现</div>;
-};
+  return (
+    <div className={classnames('component-name', styles.componentName)}>
+      组件 ComponentName 待实现
+    </div>
+  )
+}
